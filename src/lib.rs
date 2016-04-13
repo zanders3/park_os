2a52ca6022a294c89d1986bbb540c4d520dbbe70
Alex Parker <3zanders@gmail.com>
2016-04-13 23:13:48 +0100

    Hello world vga driver

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-	let hello = b"Hello World!";
-	let color_byte = 0x1f;
-	let mut hello_coloured = [color_byte; 24];
-	for (i, char_byte) in hello.into_iter().enumerate() {
-		hello_coloured[i*2] = *char_byte;
-	}
-
-	let buffer_ptr = (0xb8000 + 1988) as *mut _;
-	unsafe { *buffer_ptr = hello_coloured };
+	vga_buffer::clear_screen();
+	println!("Hello World{}", "!");
+	loop {}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
